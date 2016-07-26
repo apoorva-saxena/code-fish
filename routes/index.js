a10f11c0a1e4674b97ed99f6a7749e39c95468b6
Carlos <chdezmar@gmail.com>
2016-07-26 10:27:01 +0100

    up to chat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/requests/new', function(req, res, next) {
+  res.render('requests/new');
+});
+
+router.post('/requests/new', function(req, res, next) {
+  var request = {
+    description: req.body.description
+  };
+  res.redirect('/chats/new');
+});
+
+
+router.get('/chats/new', function(req, res, next) {
+  res.render('chats/new');
 });
 
 module.exports = router;
