8e1b98927fd1719b855b543602c39f3e0aa4226d
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-07-26 15:02:33 +0100

    added room ID's appearing on index page - but not quite working yet

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,16 +1,28 @@
 var socket = io();
 
 $('#request-form').submit(function(){
+    console.log('before called host room');
   socket.emit('host room', { description: $('#request-description').val() });
+  console.log('after called host room');
 });
 
 
 
-$('form').submit(function(){
+$('#chatbox').submit(function(){
   socket.emit('chat message', $('#m').val());
 $('#m').val('');
   return false;
 });
 socket.on('chat message', function(msg){
   $('#messages').append($('<li>').text(msg));
+});
+
+// $(document).ready(function(){
+//     $('.rooms').append('<li> test </li>');
+// });
+
+socket.on('new room', function(data){
+  console.log(data.roomID);
+  console.log('appending string process running');
+  $('#rooms').append($('<li>').text(data.roomID));
 });
