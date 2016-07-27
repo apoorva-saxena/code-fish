e65b19c5dc922d013d7f7e01e749f3585d33bef0
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-28 00:57:56 +0100

    getting user by id :zap:

--- models/user.js
+++ models/user.js
@@ -29,6 +29,10 @@ module.exports.getUserByUsername = function(username, callback) {
     User.findOne(query, callback);
 };
 
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+};
+
 module.exports.comparePassword = function(candidatePassword, hash, callback) {
     bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
         if (err) throw err;
