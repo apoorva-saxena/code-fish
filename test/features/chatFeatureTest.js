77ec7f63be8e0deae9b39ff0ae8fe20ec7dffebe
riyapabari <riya.pabari@gmail.com>
2016-07-27 19:18:58 +0100

    added travis and coveralls :honeybess:

--- test/features/chatFeatureTest.js
+++ test/features/chatFeatureTest.js
@@ -1,18 +1,13 @@
 process.env.NODE_ENV = 'test';
 var app = require('../../app');
 var Browser = require('zombie');
-var http = require('http');
 var expect = require('chai').expect;
-// var server = http.createServer(app);
-// require('socket.io').listen(server);
 
-var browser1;
-var browser2;
+var browser1, browser2;
 
 describe('chat feature', function() {
 
   beforeEach(function(done) {
-    // app.listen(3001);
     browser1 = new Browser({ site: "http://localhost:3000"});
     browser2 = new Browser({ site: "http://localhost:3000"});
     done();
@@ -20,33 +15,22 @@ describe('chat feature', function() {
 
   beforeEach(function(done) {
     browser1.visit('/', done);
-
   });
 
   beforeEach(function(done) {
     browser2.visit('/', done);
-
   });
 
-  afterEach(function(done) {
-    // server.close();
-    done();
-  });
-
-  describe('page load', function() {
-    it('has no requests to start with', function(done) {
-      setTimeout(function(){
-
-
+  // describe('page load', function() {
+    
+    it('has no requests to start with', function() {
       expect(browser1.text()).to.contain('Currently no requests');
-      done();
-    },0);
+    });
 
-    }) ;
 
     // it('initiates a chat between two people', function() {
     //   browser.pressButton('Ask for help');
     // });
-  });
+  // });
 
 });
