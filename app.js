73b641e9860e05bccfc462e987df2977648c0de1
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-27 12:03:10 +0100

    first test passing :zap:

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
@@ -33,6 +34,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressValidator());
+app.use('test', function() {
+  port = 3001;
+});
 
 app.use(session({
   secret: 'secret',
--- test/features/userFeatureTest.js
+++ test/features/userFeatureTest.js
@@ -1,60 +1,27 @@
+process.env.NODE_ENV = 'test';
 var app = require('../../app');
+
 var Browser = require('zombie');
 var http = require('http');
 var server = http.createServer(app);
 
-describe('User visits signup page', function() {
+var browser;
 
-  var browser = new Browser();
+
+describe('User visits signup page', function() {
 
   beforeEach(function(done) {
     server.listen(3001);
-    browser = new Browser({site: 'http://localhost:3001'});
-    browser.visit('/', done);
+    browser = new Browser({ site: "http://localhost:3001"});
+    browser.visit('/users/signup', done);
+  });
+  afterEach(function(done) {
+    server.close(done);
   });
 
-  afterEach(function() {
-    server.close();
-  });
-
-  describe('Submits form', function() {
-
-    // beforeEach(function(done) {
-    //   browser.visit('/users/signup', done);
-    // });
-
-    it('Should see the sign up page', function() {
-      browser.fetch('/users/signup').then(function() {
+  describe('Should see the signup page', function() {
+    it('Should see sign up', function() {
       browser.assert.text('h2', 'Sign up');
     });
   });
-
-    it('Should be successful', function() {
-        browser.fetch('/users/signup').then(function() {
-        browser.fill('username', 'hello123')
-          .fill('email', 'hello@hello.com')
-          .fill('password', 'hello123')
-          .fill('passwordconfirmation', 'hello123')
-          .pressButton('Sign Up', function(){
-        browser.assert.text('flash_msg', 'You are registered and can now log in');
-        });
-      });
-      });
-
-
-    it('Should not be successful', function() {
-        browser.fetch('/users/signup').then(function() {
-        browser.fill('username', '')
-          .fill('email', 'hello@hello.com')
-          .fill('password', 'hello123')
-          .fill('passwordconfirmation', 'hello123')
-          .pressButton('Sign Up', function(){
-        browser.assert.text('flash_msg', 'Username is required');
-        });
-      });
-      });
-
-
-  });
-
 });
