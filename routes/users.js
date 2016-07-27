48335b98136a5ddc087dde48609ae8a18cfe350d
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-28 00:56:53 +0100

    serialize added :zap:

--- routes/users.js
+++ routes/users.js
@@ -76,6 +76,17 @@ passport.use(new LocalStrategy(
         });
     }));
 
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
 router.post('/signin',
     passport.authenticate('local', {
         successRedirect: '/',
