69a0198cf2d3c3a8a00aa0a8b3ce1720f1293319
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-28 00:06:02 +0100

    passport continued :zap:

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var connect = require('connect');
 
 
 /* GET users listing. */
