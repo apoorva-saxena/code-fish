812720e616990d5aaf42aec73587783a0e282b90
toby676 <tjcmui@gmail.com>
2016-07-29 12:17:55 +0100

    make test suite cleaner :boom:

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', {currentUser: res.locals.user});
+  res.render('index');
 });
 
 module.exports = router;
