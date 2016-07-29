23fd67cb011679ad379a4dcc4c0c8320bfa4f7db
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-29 18:08:21 +0100

    profile test in progress :zap:

--- test/features/userSignInFeatureTest.js
+++ test/features/userSignInFeatureTest.js
@@ -1,68 +1,68 @@
-process.env.NODE_ENV = 'test';
-var app = require('../../app');
-var Browser = require('zombie');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
-var expect = require('chai').expect;
-
-
-
-var browser;
-
-
-describe('User visits login page', function() {
-
-  beforeEach(function(done) {
-    browser = new Browser({ site: "http://localhost:3000"});
-    browser.visit('/sessions/new', done);
-  });
-  afterEach(function(done) {
-    mongoose.connection.db.dropDatabase();
-    // app.close(done);
-    done();
-  });
-
-  describe("User cannot Sign in with the wrong password", function(){
-
-    beforeEach(function(done){
-      var newUser = new User({
-        username: "hello",
-        email: "hello@hello.com",
-        password: "fish"
-      });
-      newUser.save();
-
-
-      browser.fill('username', 'hello');
-      browser.fill('password', 'hello');
-      browser.pressButton('Sign in', done);
-    });
-
-
-
-    it('error of invalid password', function(){
-      expect(browser.text()).to.contain('Invalid password');
-    });
-  });
-
-
-  describe("User cannot Sign in", function(){
-
-    beforeEach(function(done){
-      var newUser = new User({
-        username: "test1",
-        email: "test1@test.com",
-        password: "test123"
-      });
-      newUser.save();
-
-      browser.fill('username', 'wrongusername')
-      .fill('password', 'test123')
-      .pressButton('Sign in', done);
-    });
-
-    it('throw error if email and password does not match', function(){
-      expect(browser.text()).to.contain('Unknown User');
-    });
-  });
-});
+// process.env.NODE_ENV = 'test';
+// var app = require('../../app');
+// var Browser = require('zombie');
+// var mongoose = require('mongoose');
+// var User = mongoose.model('User');
+// var expect = require('chai').expect;
+//
+//
+//
+// var browser;
+//
+//
+// describe('User visits login page', function() {
+//
+//   beforeEach(function(done) {
+//     browser = new Browser({ site: "http://localhost:3000"});
+//     browser.visit('/sessions/new', done);
+//   });
+//   afterEach(function(done) {
+//     mongoose.connection.db.dropDatabase();
+//     // app.close(done);
+//     done();
+//   });
+//
+//   describe("User cannot Sign in with the wrong password", function(){
+//
+//     beforeEach(function(done){
+//       var newUser = new User({
+//         username: "hello",
+//         email: "hello@hello.com",
+//         password: "fish"
+//       });
+//       newUser.save();
+//
+//
+//       browser.fill('username', 'hello');
+//       browser.fill('password', 'hello');
+//       browser.pressButton('Sign in', done);
+//     });
+//
+//
+//
+//     it('error of invalid password', function(){
+//       expect(browser.text()).to.contain('Invalid password');
+//     });
+//   });
+//
+//
+//   describe("User cannot Sign in", function(){
+//
+//     beforeEach(function(done){
+//       var newUser = new User({
+//         username: "test1",
+//         email: "test1@test.com",
+//         password: "test123"
+//       });
+//       newUser.save();
+//
+//       browser.fill('username', 'wrongusername')
+//       .fill('password', 'test123')
+//       .pressButton('Sign in', done);
+//     });
+//
+//     it('throw error if email and password does not match', function(){
+//       expect(browser.text()).to.contain('Unknown User');
+//     });
+//   });
+// });
