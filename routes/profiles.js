4b5d5326d75e112456c7783b9586591d5e49de8f
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-30 20:02:20 +0100

    user profile route added :zap:

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,3 +4,15 @@ var User = require('../models/user');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var connect = require('connect');
+
+router.get('/', function(req, res, next) {
+  console.log("Inside profile");
+  User.findOne({ _id: req.user._id}, function(err, user) {
+    if(err) {
+      return next(err);
+    }
+    else {
+      res.render('users/profile', { user:user });
+    }
+  });
+});
