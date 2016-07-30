7c5df9a8a34d10d0cd006be5ac887c27242a4bef
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-30 19:44:53 +0100

    profile routes added :zap:

---
+++ routes/profiles.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var connect = require('connect');
