1416f8e20987a3b8cf018e44cb012ea9492de90d
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-08-02 17:23:07 +0100

    weve got it working on the chat page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -58,7 +58,29 @@ socket.on('new room', function(){
 });
 
 socket.on('person joined', function(data){
+
+
   $('#page-layout').html($('#chat-template').html());
+  var editor = CodeMirror.fromTextArea(document.getElementById("textit"), {
+    mode: 'javascript',
+    lineNumbers: true,
+    theme: "ambiance"
+  });
+
+  socket.on('refresh', function (data) {
+    editor.setValue(data.body);
+  });
+  socket.on('change', function (data) {
+    console.log(data);
+    editor.replaceRange(data.text, data.from, data.to);
+  });
+  editor.on('change', function (i, op) {
+    console.log(op);
+    socket.emit('change', op);
+    socket.emit('refresh', editor.getValue());
+  });
+
+
   $('#chatbox').submit(function(e){
     e.preventDefault();
     socket.emit('chat message', { roomID: data.roomID,
