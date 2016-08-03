47094f16f40f5a82d3946cddcb30c06a0232fcaa
Apoorva Saxena <apsaxena24@gmail.com>
2016-08-03 22:06:19 +0100

    Anchor on map working :zap:

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -42,7 +42,7 @@
     geocoder.geocode( {address:address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
-         var image = '../images/pearl.png';
+         var image = '../images/anchor.png';
         var marker = new google.maps.Marker({ map: map,
                                               position: results[0].geometry.location,
                                               icon: image
