59771a63a46ebfeecb68eee072901eefa165dbc0
Apoorva Saxena <apsaxena24@gmail.com>
2016-08-03 16:13:37 +0100

    db test

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/globalCodeNetwork-dev',
   test: 'mongodb://localhost/globalCodeNetwork-test',
-  PROD_MONGODB: 'mongodb://apsaxena24:simple1234@ds139655.mlab.com:39655/globalcodenetwork'
+  production: 'mongodb://apsaxena24:simple1234@ds139655.mlab.com:39655/globalcodenetwork'
 };
 
 module.exports = config;
