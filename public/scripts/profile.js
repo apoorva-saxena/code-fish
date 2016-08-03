60eac33bf361e1b8cdde300d921017a5d6709384
riyapabari <riya.pabari@gmail.com>
2016-08-03 09:51:01 +0100

    removes alert for null cities :honeybee:

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -32,9 +32,6 @@
                                               position: results[0].geometry.location
                                             });
       }
-      else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
     });
   }
 
